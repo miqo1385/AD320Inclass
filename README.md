47135681bc536319a5ea733c573a57c58f6a2810
miqo1385 <miqo1385@gmail.com>
2024-02-21 19:19:32 -0800

    nested buttons

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # AD320Inclass
+
---
+++ nested-buttons/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NestedButtons from "./NestedButtons";
+
+function App() {
+  return (
+      <div className="App">
+        <h1>Event Propagation Assignment</h1>
+        <NestedButtons />
+      </div>
+  );
+}
+
+export default App;
